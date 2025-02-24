9c9de1e67acb757531234cac2eae80c7c8c16e62
vinicio <vinicio.borja10@gmail.com>
2025-02-24 04:44:19 -0500

    Add Dockerfile for multi-stage build and remove unused imports and CORS configuration

--- src/main/java/org/borja/springcloud/msvc/clients/msclients/MsClientsApplication.java
+++ src/main/java/org/borja/springcloud/msvc/clients/msclients/MsClientsApplication.java
@@ -1,22 +1,9 @@
 package org.borja.springcloud.msvc.clients.msclients;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.borja.springcloud.msvc.clients.msclients.models.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @EnableR2dbcRepositories
 @SpringBootApplication
