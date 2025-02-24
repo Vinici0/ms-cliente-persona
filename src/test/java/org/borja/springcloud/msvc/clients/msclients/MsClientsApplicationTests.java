b930c00e54d68be0d7a94206f35fc03ba94be6e4
vinicio <vinicio.borja10@gmail.com>
2025-02-23 19:18:21 -0500

    Add unit test for MsClientsApplication context loading

--- src/test/java/org/borja/springcloud/msvc/clients/msclients/MsClientsApplicationTests.java
+++ src/test/java/org/borja/springcloud/msvc/clients/msclients/MsClientsApplicationTests.java
@@ -1,10 +1,10 @@
-package org.borja.springcloud.msvc.cliente_persona.cliente_persona;
+package org.borja.springcloud.msvc.clients.msclients;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientePersonaApplicationTests {
+class MsClientsApplicationTests {
 
     @Test
     void contextLoads() {
